b169dd650c09b86e9933b161154eb10a7ed9cfb3
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-10-05 23:14:40 -0400

    Agrgando botones

---
+++ 2-tarea-permission/app/src/main/java/com/aip/a2_tarea_permission/ShowPermissionActivity.java
@@ -0,0 +1,13 @@
+package com.aip.a2_tarea_permission;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowPermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_permission);
+    }
+}
