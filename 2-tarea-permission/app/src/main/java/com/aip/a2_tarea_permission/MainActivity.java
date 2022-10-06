01128c03007873cbb707a9bd18e12befd38d9410
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-10-05 20:48:10 -0400

    Tarea 2.

---
+++ 2-tarea-permission/app/src/main/java/com/aip/a2_tarea_permission/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aip.a2_tarea_permission;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
