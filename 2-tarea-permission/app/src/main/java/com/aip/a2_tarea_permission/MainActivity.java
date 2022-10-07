1218314fbfc4147110f5f9cec02334407f6a3bbc
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-10-07 19:12:29 -0400

    Permisos y seguanda vista ahora si funcionando xd.

--- 2-tarea-permission/app/src/main/java/com/aip/a2_tarea_permission/MainActivity.java
+++ 2-tarea-permission/app/src/main/java/com/aip/a2_tarea_permission/MainActivity.java
@@ -29,14 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     private final ArrayList<Switch> allPermissionSwitch = new ArrayList<>();
 
-    private Map<Integer, Switch> permissionsSwitch = new HashMap<>();
+    private Map<String, String> permissionsLabel = new HashMap<>();
 
-    private static final int STORAGE_CODE = 100;
-    private static final int LOCATION_CODE = 200;
-    private static final int CAMERA_CODE = 300;
-    private static final int PHONE_CODE = 400;
-    private static final int CONTACT_CODE = 500;
-    private static final int GLOBAL_CODE = 600;
+    private static final int GLOBAL_CODE = 500;
 
 
 
@@ -52,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         SwPhone = findViewById(R.id.spn_phone);
         SwContacts = findViewById(R.id.spn_contacts);
         addAllSwitch();
+        fillMap();
         checkPermissions();
 
 
@@ -62,16 +58,16 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
             SwStorage.setChecked(true);
             SwStorage.setClickable(false);
-        } else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+        } if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             SwCamera.setChecked(true);
             SwCamera.setClickable(false);
-        } else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        } if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             SwLocation.setChecked(true);
             SwLocation.setClickable(false);
-        } else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+        } if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
             SwPhone.setChecked(true);
             SwPhone.setClickable(false);
-        } else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+        } if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
             SwContacts.setChecked(true);
             SwContacts.setClickable(false);
         }
@@ -79,87 +75,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void getInfoForPermission(View view){
 
-//        if (view.getId() == SwStorage.getId()){
-//            grantPermission(STORAGE_CODE, Manifest.permission.READ_EXTERNAL_STORAGE );
-//
-//        } else if (view.getId() == SwLocation.getId()) {
-//            grantPermission(LOCATION_CODE, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
-//
-//        } else if (view.getId() == SwCamera.getId()) {
-//            grantPermission(CAMERA_CODE, Manifest.permission.CAMERA);
-//
-//        }else if (view.getId() == SwPhone.getId()){
-//            grantPermission(PHONE_CODE, Manifest.permission.CALL_PHONE);
-//
-//        } else if (view.getId() == SwContacts.getId()) {
-//            grantPermission(CONTACT_CODE, Manifest.permission.READ_CONTACTS);
-//
-//        }
-        Map<Integer, String> map = new HashMap<>();
         ArrayList<String> perms = new ArrayList<>();
 
         if (SwStorage.isChecked()){
-            map.put(STORAGE_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
-            perms.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-            //grantPermission(STORAGE_CODE, Manifest.permission.READ_EXTERNAL_STORAGE );
 
+            perms.add(Manifest.permission.READ_EXTERNAL_STORAGE);
 
         } if (SwLocation.isChecked()) {
-            map.put(LOCATION_CODE, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
-            perms.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
-            //grantPermission(LOCATION_CODE, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
 
+            perms.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
 
         } if ( SwCamera.isChecked()) {
-            map.put(CAMERA_CODE, Manifest.permission.CAMERA);
+
             perms.add(Manifest.permission.CAMERA);
-            //grantPermission(CAMERA_CODE, Manifest.permission.CAMERA);
 
 
         }if (SwPhone.isChecked()){
-            map.put(PHONE_CODE, Manifest.permission.CALL_PHONE);
-            perms.add(Manifest.permission.CALL_PHONE);
-            //grantPermission(PHONE_CODE, Manifest.permission.CALL_PHONE);
 
+            perms.add(Manifest.permission.CALL_PHONE);
 
         } if ( SwContacts.isChecked()) {
-            map.put(CONTACT_CODE, Manifest.permission.READ_CONTACTS);
+
             perms.add(Manifest.permission.READ_CONTACTS);
-            //grantPermission(CONTACT_CODE, Manifest.permission.READ_CONTACTS);
 
         }
-        if (grant(perms)){
-            Intent intent = new Intent(this, ShowPermissionActivity.class);
-            startActivity(intent);
-        }
-
-
-
-
+        grantPermissions(perms);
 
 
     }
-    public boolean grant( ArrayList<String> permissions){
-
+    public void grantPermissions( ArrayList<String> permissions){
 
         ActivityCompat.requestPermissions(MainActivity.this, permissions.toArray(new String[0]), GLOBAL_CODE);
-        return true;
+
     }
 
-
-//    public void grantPermission( Integer requestCode, String permissionType){
-//
-//        if (ContextCompat.checkSelfPermission(MainActivity.this, permissionType) == PackageManager.PERMISSION_DENIED) {
-//
-//            // Requesting the permission
-//            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permissionType }, requestCode);
-//        }
-//        else {
-//            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
-//        }
-//
-//
-//    }
 
     ///Referencia de GeeksforGeeks https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
     @Override
@@ -168,55 +117,22 @@ public class MainActivity extends AppCompatActivity {
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == GLOBAL_CODE){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            for (String pm: permissions) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-               Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
-           } else {
+//                    Toast.makeText(MainActivity.this, permissionsLabel.get(pm) +" Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
 
-               Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, permissionsLabel.get(pm)+" Permission Denied", Toast.LENGTH_SHORT).show();
+                }
             }
+
+            Intent intent = new Intent(this, ShowPermissionActivity.class);
+            startActivity(intent);
+
+
         }
-//        if (requestCode == STORAGE_CODE) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                //SwStorage.setClickable(false);
-//                Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
-//            } else {
-//                SwStorage.setChecked(false);
-//                Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
-//            }
-//        } else if (requestCode == LOCATION_CODE) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                //SwLocation.setClickable(false);
-//                Toast.makeText(MainActivity.this, "Location Permission Granted", Toast.LENGTH_SHORT).show();
-//            } else {
-//                //SwLocation.setChecked(false);
-//                Toast.makeText(MainActivity.this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
-//            }
-//        } else if (requestCode == CAMERA_CODE) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                //.setClickable(false);
-//                Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
-//            } else {
-//                //SwCamera.setChecked(false);
-//                Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
-//            }
-//        } else if (requestCode == PHONE_CODE) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                //SwPhone.setClickable(false);
-//                Toast.makeText(MainActivity.this, "Phone Permission Granted", Toast.LENGTH_SHORT).show();
-//            } else {
-//                //SwPhone.setChecked(false);
-//                Toast.makeText(MainActivity.this, "Phone Permission Denied", Toast.LENGTH_SHORT).show();
-//            }
-//        } else if (requestCode == CONTACT_CODE) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                //SwContacts.setClickable(false);
-//                Toast.makeText(MainActivity.this, "Contacts Permission Granted", Toast.LENGTH_SHORT).show();
-//            } else {
-//                //SwContacts.setChecked(false);
-//                Toast.makeText(MainActivity.this, "Contacts Permission Denied", Toast.LENGTH_SHORT).show();
-//            }
-//        }
+
     }
     public void addAllSwitch(){
         allPermissionSwitch.add(SwPhone);
@@ -224,15 +140,14 @@ public class MainActivity extends AppCompatActivity {
         allPermissionSwitch.add(SwStorage);
         allPermissionSwitch.add(SwLocation);
         allPermissionSwitch.add(SwCamera);
-        permissionsSwitch.put(STORAGE_CODE, SwStorage);
-        permissionsSwitch.put(LOCATION_CODE, SwLocation);
-        permissionsSwitch.put(CAMERA_CODE, SwCamera);
-        permissionsSwitch.put(PHONE_CODE, SwPhone);
-        permissionsSwitch.put(CONTACT_CODE, SwContacts);
     }
 
-    public void seeInfo(View view){
-        Intent intent = new Intent(this, ShowPermissionActivity.class);
-        startActivity(intent);
+    public void fillMap(){
+        permissionsLabel.put(Manifest.permission.READ_EXTERNAL_STORAGE, "Storage");
+        permissionsLabel.put(Manifest.permission.ACCESS_BACKGROUND_LOCATION, "Location");
+        permissionsLabel.put(Manifest.permission.READ_EXTERNAL_STORAGE, "Storage");
+        permissionsLabel.put(Manifest.permission.CAMERA, "Camera");
+        permissionsLabel.put(Manifest.permission.CALL_PHONE, "Phone");
+        permissionsLabel.put(Manifest.permission.READ_CONTACTS, "Contact");
     }
 }
--- 2-tarea-permission/app/src/main/java/com/aip/a2_tarea_permission/ShowPermissionActivity.java
+++ 2-tarea-permission/app/src/main/java/com/aip/a2_tarea_permission/ShowPermissionActivity.java
@@ -1,13 +1,198 @@
 package com.aip.a2_tarea_permission;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ShowPermissionActivity extends AppCompatActivity {
+
+
+    private ArrayList<PermissionBtn> permissions = new ArrayList<>();
+    private static final int STORAGE_CODE = 100;
+    private static final int LOCATION_CODE = 200;
+    private static final int CAMERA_CODE = 300;
+    private static final int PHONE_CODE = 400;
+    private static final int CONTACT_CODE = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_permission);
+        addAllButtons();
+
     }
+    public void open(View view, PermissionBtn permissionBtn){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Permission granted. Are you sure you want to open this resource?");
+                alertDialogBuilder.setPositiveButton("yes",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                openUp(permissionBtn);
+
+                                //Toast.makeText(ShowPermissionActivity.this,"You clicked yes button",Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+        alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void checkPermissions(View view){
+        for (PermissionBtn pm: permissions) {
+            if (view.getId() == pm.getBtnPermission().getId()){
+                if (pm.getGranted()){
+                    open(view, pm);
+                }else{
+                    Toast.makeText(ShowPermissionActivity.this, "This permission is DENIED", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        }
+
+    }
+
+    public void addAllButtons(){
+        PermissionBtn permissionBtn = new PermissionBtn(), permissionBtnL = new PermissionBtn(),
+                permissionBtnC = new PermissionBtn(), permissionBtnP = new PermissionBtn(), permissionBtnCo = new PermissionBtn();
+        permissionBtn.setBtnPermission(findViewById(R.id.btn_storage));
+        permissionBtn.setPermissionType(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (ContextCompat.checkSelfPermission(ShowPermissionActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            permissionBtn.setGranted(true);
+            permissionBtn.setCode(STORAGE_CODE);
+        }
+        permissions.add(permissionBtn);
+
+        permissionBtnL.setBtnPermission(findViewById(R.id.btn_location));
+        permissionBtnL.setPermissionType(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+        if (ContextCompat.checkSelfPermission(ShowPermissionActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            permissionBtnL.setGranted(true);
+            permissionBtnL.setCode(LOCATION_CODE);
+
+        }
+        permissions.add(permissionBtnL);
+
+        permissionBtnC.setBtnPermission(findViewById(R.id.btn_camera));
+        permissionBtnC.setPermissionType(Manifest.permission.CAMERA);
+        if (ContextCompat.checkSelfPermission(ShowPermissionActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            permissionBtnC.setGranted(true);
+            permissionBtnC.setCode(CAMERA_CODE);
+
+        }
+        permissions.add(permissionBtnC);
+
+        permissionBtnP.setBtnPermission(findViewById(R.id.btn_phone));
+        permissionBtnP.setPermissionType(Manifest.permission.CALL_PHONE);
+        if (ContextCompat.checkSelfPermission(ShowPermissionActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+            permissionBtnP.setGranted(true);
+            permissionBtnP.setCode(PHONE_CODE);
+        }
+        permissions.add(permissionBtnP);
+
+        permissionBtnCo.setBtnPermission(findViewById(R.id.btn_contacts));
+        permissionBtnCo.setPermissionType(Manifest.permission.READ_CONTACTS);
+        if (ContextCompat.checkSelfPermission(ShowPermissionActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+            permissionBtnCo.setGranted(true);
+            permissionBtnCo.setCode(CONTACT_CODE);
+        }
+        permissions.add(permissionBtnCo);
+
+
+
+    }
+
+    public void openUp(PermissionBtn permissionBtn){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        String phoneNumber = "829-613-3107";
+        if (permissionBtn.getCode() == STORAGE_CODE){
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.setType("file/*");
+            startActivity(intent);
+        } else if (permissionBtn.getCode() == LOCATION_CODE) {
+            // Create a Uri from an intent string. Use the result to create an Intent.
+            Uri gmmIntentUri = Uri.parse("geo:40.7396924,-74.0359998");
+            intent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            intent.setPackage("com.google.android.apps.maps");
+            startActivity(intent);
+
+            startActivity(intent);
+        } else if (permissionBtn.getCode() == CAMERA_CODE) {
+            intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(intent);
+            
+        } else if (permissionBtn.getCode() == PHONE_CODE) {
+            intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" +phoneNumber));
+            startActivity(intent);
+
+        } else if (permissionBtn.getCode() == CONTACT_CODE) {
+            intent= new Intent(Intent.ACTION_PICK,  ContactsContract.Contacts.CONTENT_URI);
+            startActivity(intent);
+
+        }
+
+    }
+
+    public static class PermissionBtn {
+        public Button btnPermission;
+        public String permissionType;
+
+        public Boolean granted = false;
+
+        public Integer code = 0;
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+
+        public Button getBtnPermission() {
+            return btnPermission;
+        }
+
+        public Boolean getGranted() {
+            return granted;
+        }
+
+        public void setGranted(Boolean granted) {
+            this.granted = granted;
+        }
+
+        public String getPermissionType() {
+            return permissionType;
+        }
+
+        public void setBtnPermission(Button btnPermission) {
+            this.btnPermission = btnPermission;
+        }
+
+        public void setPermissionType(String permissionType) {
+            this.permissionType = permissionType;
+        }
+    }
+
 }
